51c906ced6f010cd6db2c7ce02f0d970cdb31ac4
Lokesh-ramesh <95431053+Lokesh-ramesh@users.noreply.github.com>
2022-07-20 23:23:55 +0530

    Update tower.c

--- tower.c
+++ tower.c
@@ -1,22 +1,36 @@
-#include<stdio.h>
-void tower(int n,char sour,char temp,char dest)
-{
-    if(n==1)
-    {
-        printf("Move disc 1 from %c to %c\n",sour,dest);
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
 
+void delay(){
+    long i,j;
+    for(i=0;i<1000;i++){
+        for(j=0;j<100000;j++);
     }
-    else{
-        tower(n-1,sour,dest,temp);
-        printf("Move %d disc from %c to %c\n",n,sour,dest);
-        tower(n-1,temp,sour,dest);
-        }
 }
-int main()
-{
+
+void hanoi(int n, char a,char b,char c) {
+    if(n==1){
+        printf("move from %c to %c\n",a,c);
+    }
+    else {
+        hanoi(n-1,a,c,b);
+        printf("move from %c to %c\n",a,c);
+        delay();
+        hanoi(n-1,b,a,c);
+    }
+}
+int main() {
+    clock_t start,end;
     int n;
-    printf("enter the number of disc\n");
+    int moves;
+    printf("Enter the number of disks\n");
     scanf("%d",&n);
-    tower(n,'S','T','D');
+    moves = pow(2,n)-1;
+    printf("Number of moves is %d\n",moves);
+    start= clock();
+    hanoi(n,'a','b','c');
+    end=clock();
+    printf("%lf seconds",(double)(end-start)/CLOCKS_PER_SEC);
     return 0;
 }

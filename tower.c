3fd4f84c4433c5f2799cb64a2e360c46b79d76a4
Lokesh-ramesh <95431053+Lokesh-ramesh@users.noreply.github.com>
2022-07-23 11:54:11 +0530

    Update tower.c

--- tower.c
+++ tower.c
@@ -1,36 +1,26 @@
-#include <stdio.h>
-#include <time.h>
-#include <math.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
+#include<time.h>
 
-void delay(){
-    long i,j;
-    for(i=0;i<1000;i++){
-        for(j=0;j<100000;j++);
+void hanoi(int x,char from,char to,char aux)
+{
+    if(x==1)
+    printf("\n move disk from %c to %c",from,to);
+    else
+    {
+        hanoi(x-1,from,aux,to);
+        printf("\n move disk from %c to %c",from,to);
+        hanoi(x-1,aux,to,from);
     }
 }
-
-void hanoi(int n, char a,char b,char c) {
-    if(n==1){
-        printf("move from %c to %c\n",a,c);
-    }
-    else {
-        hanoi(n-1,a,c,b);
-        printf("move from %c to %c\n",a,c);
-        delay();
-        hanoi(n-1,b,a,c);
-    }
-}
-int main() {
-    clock_t start,end;
-    int n;
-    int moves;
-    printf("Enter the number of disks\n");
-    scanf("%d",&n);
-    moves = pow(2,n)-1;
-    printf("Number of moves is %d\n",moves);
-    start= clock();
-    hanoi(n,'a','b','c');
-    end=clock();
-    printf("%lf seconds",(double)(end-start)/CLOCKS_PER_SEC);
+int main()
+{
+    int disks;int moves;
+    printf("enter number of disks\n");
+    scanf("%d",&disks);
+    moves=pow(2,disks)-1;
+    printf("the number of moves required is %d",moves);
+    hanoi(disks,'a','b','c');
     return 0;
 }

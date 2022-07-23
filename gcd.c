dcd7cf7ceb4f52855a0ebb6898b5e0b6007bc7fb
Lokesh-ramesh <95431053+Lokesh-ramesh@users.noreply.github.com>
2022-07-23 11:54:34 +0530

    Update gcd.c

--- gcd.c
+++ gcd.c
@@ -1,33 +1,21 @@
-#include <stdio.h>
-#include <time.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<conio.h>
 
-void delay();
-int gcd(int x,int y);
+int hcf(int x,int y);
 
-void main()
+int main()
+{int x,y;
+    printf("enter two numbers\n");
+    scanf("%d %d",&x,&y);
+    printf("gcd of numbers is %d",(hcf(x,y)));
+    return 0;
+}
+int hcf(int x,int y)
 {
-    int a,b,c;
-    clock_t start,end;
-    printf("Enter two numbers: ");
-    scanf("%d%d",&a,&b);
-    start= clock();
-    delay();
-    c=gcd(a,b);
-    end= clock();
-    printf("The gcd of two numbers is %d\n",c);
-    printf("The time taken is %lf seconds",((double)(end-start)/CLOCKS_PER_SEC));
-}
-void delay(){
-    long i;
-    for(i=0;i<10000000;i++);
-}
-int gcd(int x,int y){
-    int z;
-    if(y==0)
+    if(y!=0)
+    
+        return hcf(y,x%y);
+        else
         return x;
-    z=x%y;
-    x=y;
-    y=z;
-    delay();
-    return gcd(x,y);
-}
+    }
